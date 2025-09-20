1091e87c1fc129b945668b73f78e4f603e809ea4
medhastuti <medhastuti@gmail.com>
2025-09-20 23:52:17 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,41 @@
-import React from 'react'
-import { Routes, Route } from 'react-router-dom'
+import React, { useState, useEffect } from 'react'
+import { Routes, Route, useLocation } from 'react-router-dom'
 import Sidebar from './components/Sidebar'
 import Topbar from './components/Topbar'
 import Dashboard from './components/Dashboard'
 import PosterPage from './components/PosterPage'
 import SocialGenerator from './components/SocialGenerator'
 import BioGenerator from './components/BioGenerator'
+import { useWindowWidth } from './components/useWindowWidth'
 
-export default function App(){
+export default function App() {
+  const windowWidth = useWindowWidth()
+  const isMobile = windowWidth < 768
+
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false)
+  const location = useLocation()
+
+  // Close sidebar on mobile when route changes (navigate)
+  useEffect(() => {
+    if (isMobile) setIsSidebarOpen(false)
+  }, [location.pathname, isMobile])
+
+  const sidebarOpen = isMobile ? isSidebarOpen : true
+
+  const toggleSidebar = () => {
+    if (isMobile) setIsSidebarOpen(!isSidebarOpen)
+  }
+
   return (
-    <div className='min-h-screen flex bg-slate-50 text-slate-800'>
-      <Sidebar />
-      <div className='flex-1 p-6'>
-        <Topbar />
+    <div className="min-h-screen flex bg-slate-50 text-slate-800">
+      <Sidebar isOpen={sidebarOpen} toggleSidebar={toggleSidebar} isMobile={isMobile} />
+      <div className="flex-1 p-6">
+        <Topbar toggleSidebar={toggleSidebar} isMobile={isMobile} />
         <Routes>
-          <Route path='/' element={<Dashboard />} />
-          <Route path='/poster' element={<PosterPage />} />
-          <Route path='/social' element={<SocialGenerator />} />
-          <Route path='/bios' element={<BioGenerator />} />
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/poster" element={<PosterPage />} />
+          <Route path="/social" element={<SocialGenerator />} />
+          <Route path="/bios" element={<BioGenerator />} />
         </Routes>
       </div>
     </div>
